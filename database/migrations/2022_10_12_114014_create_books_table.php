3a33232f16b8ec6226c9167cf4ca221c24355560
Samuel Lujan <samuel_lujan@hotmail.com>
2022-10-12 08:52:42 -0300

    feat: criando migrations de livros e indices

---
+++ database/migrations/2022_10_12_114014_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, 'publisher_user_id')->constrained();
+            $table->string('title', 300);
+            $table->text('raw_text')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ database/migrations/2022_10_12_114333_create_book_indices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\BookIndex;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_indices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Book::class, 'book_id')->constrained();
+            $table->foreignIdFor(BookIndex::class, 'index_id')->constrained()->nullable();
+            $table->string('title', 255);
+            $table->integer('page', false, true)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_indices');
+    }
+};
