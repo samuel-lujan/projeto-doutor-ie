eb5270d0e695c45e77d5c5bc15680703b55bdac1
Samuel Lujan <samuel_lujan@hotmail.com>
2022-10-12 13:01:56 -0300

    feat: adicionando configurações da biblioteca media library

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Book extends Model
+class Book extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     /**
      * The attributes that are mass assignable.
