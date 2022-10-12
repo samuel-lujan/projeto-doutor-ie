f31b0a56d7c71f1f300e6826ff60c8571957f8db
Samuel Lujan <samuel_lujan@hotmail.com>
2022-10-12 12:56:06 -0300

    feat: criando métodos para criar e atualizar livros

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,67 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Books\StoreBookRequest;
+use App\Models\Book;
+use App\Services\BookIndex\BookServiceIndex;
+use App\Services\Books\BookService;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class BookController extends Controller
 {
-    //
+    private BookService $bookService;
+    private BookServiceIndex $bookIndexService;
+
+    public function __construct(BookService $bookService, BookServiceIndex $bookIndexService)
+    {
+        $this->bookService = $bookService;
+        $this->bookIndexService = $bookIndexService;
+    }
+
+    public function store(StoreBookRequest $request) {
+        $attributes = $request->all();
+
+        try {
+            $bookResponse = $this->bookService->create($attributes);
+            $book = $bookResponse['data'];
+            $this->bookIndexService->create($attributes['indices'], $book);
+
+        } catch (Exception $e) {
+            Log::error("Houve uma falha ao criar o livro {$e->getMessage()}", $e->getTrace());
+            return response()->json([
+                'success' => false,
+                'message' => 'Houve uma falha ao criar o livro',
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Livro cadastrado com sucesso',
+            'data' => $book,
+        ], 201);
+    }
+
+    public function update(StoreBookRequest $request, Book $book) {
+        $attributes = $request->validated();
+
+        try {
+            $bookResponse = $this->bookService->update($attributes, $book);
+            $book = $bookResponse['data'];
+            $this->bookIndexService->update($attributes['indices'], $book);
+
+        } catch (Exception $e) {
+            Log::error("Houve uma falha ao editar o livro {$e->getMessage()}", $e->getTrace());
+            return response()->json([
+                'success' => false,
+                'message' => 'Houve uma falha ao editar o livro',
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Livro editado com sucesso',
+            'data' => $book,
+        ], 200);
+    }
 }
